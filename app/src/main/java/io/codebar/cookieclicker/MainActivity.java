6177a08832667e33178cc5c29ac79a2848fe9123
Matt Oakes <hello@mattoakes.net>
2017-05-09 11:05:07 +0100

    Initial commit

---
+++ app/src/main/java/io/codebar/cookieclicker/MainActivity.java
@@ -0,0 +1,34 @@
+package io.codebar.cookieclicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView counterTextView;
+    private int currentScore = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView cookieImage = (ImageView) findViewById(R.id.image_cookie);
+        cookieImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentScore++;
+                updateCounter();
+            }
+        });
+
+        counterTextView = (TextView) findViewById(R.id.counter);
+        updateCounter();
+    }
+
+    private void updateCounter() {
+        counterTextView.setText(String.valueOf(currentScore));
+    }
+}
